5e3ed4c65706bbb546596e10b80521ad144a6ae0
ItsJonQ <itsjonq@gmail.com>
2014-07-14 21:02:44 -0400

    WIP: Fixed button mixin logic

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
         expand: true,
         cwd: 'dist',
         src: ['**/*.scss'],
-        dest: '',
+        dest: 'build',
         ext: '.css'
       }
     },
